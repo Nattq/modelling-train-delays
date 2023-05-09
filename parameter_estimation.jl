6e64bcec77e080a06f738e070d666ea6db0a0f6d
Klaudia Jaworek <kjaworek26@gmail.com>
2023-05-09 17:57:24 +0200

    Estymacja dla danych

--- parameter_estimation.jl
+++ parameter_estimation.jl
@@ -1,6 +1,8 @@
 using Evolutionary
 using Statistics
 using Random
+using XLSX
+using DataFrames
 
 function rungekutta4(f::Function, x::LinRange, initial_condition::Vector, params::Vector = [])::Tuple{LinRange, Array}
     n = length(x) - 1
@@ -26,24 +28,39 @@ function SIR_derivatives(SIR_numbers::Vector{Float64}, β::Number, γ::Number)::
     [dS, dI, dR]
 end
 
-function mean_relative_error(xs::Vector{Float64}, ys::Vector{Float64})::Number 
+function mean_relative_error(xs, ys)::Number 
     mean(abs.(xs .- ys)./xs)
 end
 
-function fitness_func(solution)
-    _, s = rungekutta4(SIR_derivatives, LinRange(0.0, 109.0, 1091), [235.0, 14.0, 0.0], solution)
+function fitness_func(solution, T, S₀, I₀, I)
+    _, s = rungekutta4(SIR_derivatives, LinRange(0.0, T, 10T+1), [S₀, I₀, 0.0], solution)
     I_m = s[:, 2]
     mean_relative_error(I, I_m)
 end
 
-β = 0.2 
-γ = 0.1
-x, data = rungekutta4(SIR_derivatives, LinRange(0.0, 109.0, 1091), [235.0, 14.0, 0.0], [β, γ])
-I = data[:, 2]
+df = DataFrame(XLSX.readtable("data//df_AC_zgrupowane.xlsx", "Sheet1"))
+df[:, "beta"] = Vector{Float64}(undef, nrow(df))
+df[:, "gamma"] = Vector{Float64}(undef, nrow(df))
+df[:, "błąd_dopasowania"] = Vector{Float64}(undef, nrow(df))
 
-Evolutionary.optimize(
-    fitness_func, 
-    BoxConstraints([0.0, 0.0], [1.0, 1.0]),
-    rand(2),
-    GA(populationSize = 100, selection = susinv, crossover = AX, mutation = uniform(0.1), mutationRate = 0.5, ε = 0.1)
-)
+for (row_number, row) in enumerate(eachrow(df))
+    I₀ = row["czy_opoznienie_wyjazd"]
+    Iₜ = row["czy_opoznienie_przyjazd"]
+    S₀ = row["nazwa_pociagu"] - I₀
+    T = row["czas_przejazdu_super_krawedz"]
+    I = (Iₜ - I₀)/T .* LinRange(0.0, T, 10T+1) .+ I₀
+    if I₀ != Iₜ
+        println(row_number) #TEST
+        solution = Evolutionary.optimize(
+            x -> fitness_func(x, T, S₀, I₀, I), 
+            BoxConstraints([0.0, 0.0], [1.0, 1.0]),
+            rand(2),
+            GA(populationSize = 100, selection = susinv, crossover = AX, mutation = uniform(0.1), mutationRate = 0.5, ε = 0.1)
+        )
+        df[row_number, "beta"] = solution.minimizer[1]
+        df[row_number, "gamma"] = solution.minimizer[2]
+        df[row_number, "błąd_dopasowania"] = solution.minimum
+    end
+end
+
+XLSX.writetable("data/parametry.xlsx", df)
